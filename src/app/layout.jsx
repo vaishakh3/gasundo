21f3235dc61498ed04e8d12d24b672eaad8c97c7
vaishakh3 <vichured@gmail.com>
2026-03-14 00:54:54 +0530

    faviicon google search

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -148,13 +148,11 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-      { url: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
     ],
-    apple: {
-      url: '/icons/apple-touch-icon.png',
-      sizes: '180x180',
-      type: 'image/png',
-    },
+    apple: [
+      { url: '/icons/icon-192.png' },
+      { url: '/icons/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
   },
 }
 
